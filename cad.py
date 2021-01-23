29833b61823a9e5abf3e0504e140e959be72901b
daniil.dubin <canddoo@gmail.com>
2021-01-23 20:35:47 +0200

    nozzle generation as a function

--- cad.py
+++ cad.py
@@ -1,24 +1,8 @@
 import math
 import Part
 import MeshPart
-
-rt = 10.87 / 2
-inflection_angle_deg = 30.0
-exit_angle_deg = 12
-nozzle_expansion_ratio = 3.7
-
-delta = 0.01
-
-exit_radius = math.sqrt(rt * rt * nozzle_expansion_ratio)
-
-outside_radius = 55.0 / 2
-
-# converging_radius_koef = 1.5
-diverging_radius_koef = 0.382
-
-inflection_angle = math.radians(inflection_angle_deg)
-exit_angle = math.radians(exit_angle_deg)
-diverging_radius = diverging_radius_koef * rt
+import FreeCAD as App
+import FreeCADGui as Gui
 
 
 def sqr(x):
@@ -39,67 +23,88 @@ def parabola_koef(infliction_angle, exit_angle, exit_radius, inflection_point):
     c = inflection_point[0] - sqr(inflection_point[1]) * a - inflection_point[1] * b
     return a, b, c
 
-xy_i = circle_point(inflection_angle, diverging_radius, 0, (1 + diverging_radius_koef) * rt)
-abc = parabola_koef(inflection_angle, exit_angle, exit_radius, xy_i)
 
-# converging part
+def build_nozzle(
+        throat_radius=10.87 / 2,
+        inflection_angle_deg=30.0,
+        exit_angle_deg=12,
+        nozzle_expansion_ratio=3.7,
+        outside_radius=55.0 / 2,
+        delta=0.25
+):
+    exit_radius = math.sqrt(throat_radius * throat_radius * nozzle_expansion_ratio)
 
-y = outside_radius
-converging_radius = outside_radius - rt
+    # converging_radius_koef = 1.5
+    diverging_radius_koef = 0.382
 
-px = -converging_radius
-# py = outside_radius
-py = 0
+    inflection_angle = math.radians(inflection_angle_deg)
+    exit_angle = math.radians(exit_angle_deg)
+    diverging_radius = diverging_radius_koef * throat_radius
 
-edges = []
+    inflection_point = circle_point(inflection_angle, diverging_radius, 0, (1 + diverging_radius_koef) * throat_radius)
+    abc = parabola_koef(inflection_angle, exit_angle, exit_radius, inflection_point)
 
-x = 0
+    # converging part
 
-while True:
-    y = y - delta
+    y = outside_radius
+    converging_radius = outside_radius - throat_radius
 
-    if y < rt:
-        break
-    else:
-        x = - math.sqrt(sqr(converging_radius) - sqr(y - outside_radius))
+    px = -converging_radius
+    # py = outside_radius
+    py = 0
 
-    edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
-    px = x
-    py = y
+    edges = []
 
-# diverging part
-while True:
-    y = py + delta
+    x = 0
 
-    if y > exit_radius:
-        break
-    else:
-        if y < xy_i[1]:
-            # circle
-            x = math.sqrt(sqr(diverging_radius) - sqr(y - (rt + diverging_radius)))
+    while True:
+        y = y - delta
+
+        if y < throat_radius:
+            break
         else:
-            # parabola
-            x = abc[0] * sqr(y) + abc[1] * y + abc[2]
+            x = - math.sqrt(sqr(converging_radius) - sqr(y - outside_radius))
 
-    edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
-    px = x
-    py = y
+        edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
+        px = x
+        py = y
 
-# back to the axis
-edges.append(Part.makeLine((px, py, 0), (px, 0, 0)))
-edges.append(Part.makeLine((px, 0, 0), (-converging_radius, 0, 0)))
+    # diverging part
+    while True:
+        y = py + delta
 
-nozzle_contour = Part.Wire(edges)
-nozzle_face = Part.Face(nozzle_contour)
+        if y > exit_radius:
+            break
+        else:
+            if y < inflection_point[1]:
+                # circle
+                x = math.sqrt(sqr(diverging_radius) - sqr(y - (throat_radius + diverging_radius)))
+            else:
+                # parabola
+                x = abc[0] * sqr(y) + abc[1] * y + abc[2]
 
-nozzle = nozzle_face.revolve(FreeCAD.Vector(1, 0, 0), FreeCAD.Vector(360, 0, 0))
+        edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
+        px = x
+        py = y
+
+    # back to the axis
+    edges.append(Part.makeLine((px, py, 0), (px, 0, 0)))
+    edges.append(Part.makeLine((px, 0, 0), (-converging_radius, 0, 0)))
+
+    nozzle_contour = Part.Wire(edges)
+    nozzle_face = Part.Face(nozzle_contour)
+
+    return nozzle_face.revolve(App.Vector(1, 0, 0), App.Vector(360, 0, 0))
+
+
+nozzle = build_nozzle()
 
 App.newDocument("Nozzle")
 App.setActiveDocument("Nozzle")
 
-nozzle_mesh = FreeCAD.ActiveDocument.addObject("Mesh::Feature", "NozzleMesh")
+nozzle_mesh = App.ActiveDocument.addObject("Mesh::Feature", "NozzleMesh")
 nozzle_mesh.Mesh = MeshPart.meshFromShape(Shape=nozzle, MaxLength=1)
-nozzle_mesh.ViewObject.DisplayMode = "Flat Lines"
+# nozzle_mesh.ViewObject.DisplayMode = "Flat Lines"
 
 # Part.show(nozzle_mesh)
 
