f8b556e1c50ad6553b9d77501858f652dc03c31d
daniil.dubin <canddoo@gmail.com>
2021-01-24 12:56:28 +0200

    more precise circle curvature (circle as function of angle)

--- cad.py
+++ cad.py
@@ -24,13 +24,19 @@ def parabola_koef(infliction_angle, exit_angle, exit_radius, inflection_point):
     return a, b, c
 
 
+# def arc(center_xy, radius, start_angle, end_angle):
+#     return Part.ArcOfCircle(Part.Circle(App.Vector(center_xy[0], center_xy[1], 0),
+#                                  App.Vector(0, 0, 1), radius),
+#                      start_angle, end_angle)
+
 def build_nozzle(
         throat_radius=10.87 / 2,
         inflection_angle_deg=30.0,
         exit_angle_deg=12,
         nozzle_expansion_ratio=3.7,
         outside_radius=55.0 / 2,
-        delta=0.25
+        delta=0.25,
+        delta_phi=math.radians(1)
 ):
     exit_radius = math.sqrt(throat_radius * throat_radius * nozzle_expansion_ratio)
 
@@ -43,46 +49,51 @@ def build_nozzle(
     diverging_radius = diverging_radius_koef * throat_radius
 
     inflection_point = circle_point(inflection_angle, diverging_radius, 0, (1 + diverging_radius_koef) * throat_radius)
-    abc = parabola_koef(inflection_angle, exit_angle, exit_radius, inflection_point)
-
-    # converging part
-
-    converging_radius = throat_radius * converging_radius_koef
-    circle_center = 0, throat_radius + converging_radius
 
     edges = []
 
+    # converging arc
+
+    converging_radius = throat_radius * converging_radius_koef
+    converging_circle_center = 0, throat_radius + converging_radius
+
     x = 0
-    y = circle_center[1]
+    y = converging_circle_center[1]
 
     px = -converging_radius
     py = 0
 
-    while True:
-        y = y - delta
+    phi = math.radians(180)
 
-        if y < throat_radius:
-            break
-        else:
-            x = - math.sqrt(sqr(converging_radius) - sqr(y - circle_center[1]))
+    while phi <= math.radians(270):
+        x = math.cos(phi) * converging_radius + converging_circle_center[0]
+        y = math.sin(phi) * converging_radius + converging_circle_center[1]
 
         edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
         px = x
         py = y
 
-    # diverging part
-    while True:
-        y = py + delta
+        phi = phi + delta_phi
 
-        if y > exit_radius:
-            break
-        else:
-            if y < inflection_point[1]:
-                # circle
-                x = math.sqrt(sqr(diverging_radius) - sqr(y - (throat_radius + diverging_radius)))
-            else:
-                # parabola
-                x = abc[0] * sqr(y) + abc[1] * y + abc[2]
+    # diverging arc
+    diverging_circle_center = 0, throat_radius + diverging_radius
+
+    while y < inflection_point[1]:
+        x = math.cos(phi) * diverging_radius + diverging_circle_center[0]
+        y = math.sin(phi) * diverging_radius + diverging_circle_center[1]
+
+        edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
+        px = x
+        py = y
+
+        phi = phi + delta_phi
+
+    # diverging parabola
+    abc = parabola_koef(inflection_angle, exit_angle, exit_radius, inflection_point)
+
+    while y <= exit_radius:
+        y = py + delta
+        x = abc[0] * sqr(y) + abc[1] * y + abc[2]
 
         edges.append(Part.makeLine((px, py, 0), (x, y, 0)))
         px = x
